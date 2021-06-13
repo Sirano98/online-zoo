70ae6eb977773984432d3cbc3cac8e3507467228
Sirano98 <protsiuk1998@gmail.com>
2021-06-14 00:10:17 +0300

    feat: add slider for video block

--- js/zoos.js
+++ js/zoos.js
@@ -20,3 +20,26 @@ function toggleSpoiler() {
     spoiler.classList.toggle("description__spoiler-close");
     spoiler.classList.contains("description__spoiler-close") ? spoilerButton.textContent = "Read More" : spoilerButton.textContent = "Read Less";
 }
+
+const videoSlider = new Swiper(".video-slider", {
+    navigation: {
+        nextEl: '.slider__btn-right',
+        prevEl: '.slider__btn-left',
+    },
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    breakpoints: {
+        1600: {
+            slidesPerView: 4
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        }
+    }
+});
